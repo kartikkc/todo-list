c73620708b10b5b8995873ee037d4780dc16ed35
Kartik <kartikkc397@outlook.com>
2023-08-02 00:19:16 +0530

    changed the footer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 // const ejs = require("ejs");
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+"public"));
+app.use(express.static(__dirname + "public"));
 app.set("views", __dirname + "/views");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,19 +16,19 @@ const options = {
 }
 let inputs = [];
 let workItems = [];
-let title = ""; 
+let title = "";
 const day = currentDay.toLocaleDateString("en-US", options);
 app.get("/", (req, res) => {
     res.render('index', { date: day, newListItems: inputs });
 })
 app.post("/", (req, res) => {
     let listName = req.body.button;
-    if(listName =="Work"){
+    if (listName == "Work") {
         workItems.push(req.body.task);
         console.log("[Input] " + workItems);
         res.redirect("/work");
     }
-    else{
+    else {
         inputs.push(req.body.task);
         console.log("[Input] " + inputs);
         res.redirect("/");
@@ -37,7 +37,7 @@ app.post("/", (req, res) => {
 })
 
 app.get("/work", (req, res) => {
-    title="Work List"
+    title = "Work List"
     res.render('index', { date: title, newListItems: workItems });
 })
 app.post("/work", (req, res) => {
@@ -46,7 +46,7 @@ app.post("/work", (req, res) => {
     res.redirect("/work");
 })
 app.get("/about", (req, res) => {
-    title="Work List"
+    title = "Work List"
     res.render('about', { date: title, newListItems: workItems });
 })
 app.listen(process.env.PORT || 3000, () => {
